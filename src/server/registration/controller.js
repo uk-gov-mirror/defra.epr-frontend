b51cb57b998f2216ea9dd0e8ab13322ab502ac9c
Nijk <1615460+nijk@users.noreply.github.com>
2025-10-27 18:50:49 +0000

    PAE-379: render Reg/Acc data on registrations page

--- src/server/registration/controller.js
+++ src/server/registration/controller.js
@@ -1,15 +1,33 @@
+import fetch from 'node-fetch'
+
+import { config } from '#config/config.js'
+
 /**
  * @satisfies {Partial<ServerRoute>}
  */
 export const registrationController = {
-  handler(request, h) {
+  async handler(request, h) {
     const { organisationId, registrationId } = request.params
+
+    const baseUrl = config.get('eprBackendUrl')
+
+    const url = `${baseUrl}/v1/organisations/${organisationId}`
+
+    const response = await fetch(url)
+    const { accreditations, registrations } = await response.json()
+
+    const registration = registrations.find(({ id }) => id === registrationId)
+    const accreditation = accreditations.find(
+      ({ id }) => id === registration.accreditationId
+    )
 
     return h.view('registration/index', {
       pageTitle: 'Registration', // @todo use activity/site/material info
       heading: 'Registration',
       organisationId,
-      registrationId
+      registrationId,
+      registration,
+      accreditation
     })
   }
 }
